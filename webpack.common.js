33fbbd1d824df79293dec38709458b2d7d3734ee
Luiz <luizribeirocorreia09@gmail.com>
2026-05-11 08:34:27 -0300

    set initial configs

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import htmlWebpackPlugin from "html-webpack-plugin";
+import { watchFile } from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        app: "./src/index.js"
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: "production",
+        }),
+    ],
+    devServer: {
+        watchFile: ["./src/template.html"],
+    },
+
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|gif|jpeg|jpg)$/i,
+                use: "asset/resource",
+            },
+        ],
+    },
+};
+
