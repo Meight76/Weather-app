865f4308046dc3594a4490ea6c2ad77de3a5e306
Luiz <luizribeirocorreia09@gmail.com>
2026-05-11 17:17:25 -0300

    prepare to update ui when fetching weather feature

--- src/controller/location.js
+++ src/controller/location.js
@@ -1,12 +1,18 @@
 import findCityLoc from "../city/find-city.js";
-import updateWeatherUi from "../ui/update-weater.js";
+// import updateWeatherUi from "../ui/update-weater.js";
 import getWeather from "../weather/fetch-weather.js";
 
-const locInput = document.querySelector("#location-input");
 
-locInput.addEventListener("change", async (e) => {
-    const locResults = await findCityLoc(e.currentTarget.value);
-    const weather = await getWeather(...locResults);
-    updateWeatherUi(weather);
+export default () => {
+    const locInput = document.querySelector("#location-input");
+    locInput.addEventListener("change", async (e) => {
+        const locResults = await findCityLoc(e.currentTarget.value);
+        if (!locResults) return;
+        const weather = await getWeather(...locResults);
+        if (!weather) return;
+        console.log(weather);
+        // updateWeatherUi(weather);
 
-});
+    });
+}
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import template from "./index.html";
 import "./style.css";
 import findCityLoc  from "../src/city/find-city.js";
 import getWeather from "./weather/fetch-weather.js";
-// console.log(getWeather())
-(async () => {
-    const [lat, long] = await findCityLoc("São Paulo");
-    const weatherObj = await getWeather(lat, long);
-    console.log(weatherObj);
-})();
+import locationController from "./controller/location.js";
 
 document.body.innerHTML = template;
+locationController();
